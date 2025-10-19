ec7209c5d47a15c942c0a518a3bded49230ee119
Charles <79457016+chvanam@users.noreply.github.com>
2025-10-19 11:51:07 +0200

    chore: setup ci (#1)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -13,21 +13,12 @@ export default defineConfig({
 					environment: 'browser',
 					browser: {
 						enabled: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }]
+						provider: 'webdriverio',
+						instances: [{ browser: 'chrome' }]
 					},
 					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
 					exclude: ['src/lib/server/**'],
 					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
 				}
 			}
 		]
